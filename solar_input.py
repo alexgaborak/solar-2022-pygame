d39ec53b714b09a39b961065442f79aae8cd6076
fleeting-mien <fleeting_mien@proton.me>
2022-11-18 10:16:09 +0300

    write_space_objects_data_to_file

--- solar_input.py
+++ solar_input.py
@@ -4,6 +4,7 @@
 from solar_objects import Body
 from solar_vis import DrawableObject
 
+
 def read_space_objects_data_from_file(input_filename):
     """Cчитывает данные о космических объектах из файла, создаёт сами объекты
     и вызывает создание их графических образов
@@ -18,8 +19,6 @@ def read_space_objects_data_from_file(input_filename):
         for line in input_file:
             if len(line.strip()) == 0 or line[0] == '#':
                 continue  # пустые строки и строки-комментарии пропускаем
-
-            object_type = line.split()[0].lower()
             body = Body()
             parse_body_parameters(line, body)
             bodies.append(body)
@@ -54,6 +53,7 @@ def parse_body_parameters(line, body):  # parse_planet_parameters
     body.x, body.y, body.Vx, body.Vy = int(args[4]), int(args[5]), int(args[6]), int(args[7])
     # FIXME: не понимает экспоненциальную запись
 
+
 def write_space_objects_data_to_file(output_filename, objects):
     """Сохраняет данные о космических объектах в файл.
 
@@ -71,8 +71,8 @@ def write_space_objects_data_to_file(output_filename, objects):
     """
     with open(output_filename, 'w') as out_file:
         for obj in objects:
-            print(out_file, "%s %d %s %f" % ('1', 2, '3', 4.5))
-            # FIXME!
+            print(out_file, "%s %d %s %f %f %f %f %f" % (obj.type.capitalize(), obj.R, obj.color,
+                                                         obj.m, obj.x, obj.y, obj.Vx, obj.Vy))
 
 
 if __name__ == "__main__":
